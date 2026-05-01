7f9d0f3af1df51303cec61458d5c2f421646bc8b
ms5788 <ms5788@drexel.edu>
2026-05-01 19:31:31 -0400

    change ProfileList to jsx and added crud api methods

--- client/src/components/ProfileList.jsx
+++ client/src/components/ProfileList.jsx
@@ -1,22 +1,22 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-const Record = (props) => (
+const Profile = (props) => (
   <tr className="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
     <td className="p-4 align-middle [&:has([role=checkbox])]:pr-0">
-      {props.record.name}
+      {props.profile.name}
     </td>
     <td className="p-4 align-middle [&:has([role=checkbox])]:pr-0">
-      {props.record.position}
+      {props.profile.position}
     </td>
     <td className="p-4 align-middle [&:has([role=checkbox])]:pr-0">
-      {props.record.level}
+      {props.profile.level}
     </td>
     <td className="p-4 align-middle [&:has([role=checkbox])]:pr-0">
       <div className="flex gap-2">
         <Link
           className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3"
-          to={`/edit/${props.record._id}`}
+          to={`/edit/${props.profile._id}`}
         >
           Edit
         </Link>
@@ -25,7 +25,7 @@ const Record = (props) => (
           color="red"
           type="button"
           onClick={() => {
-            props.deleteRecord(props.record._id);
+            props.deleteProfile(props.profile._id);
           }}
         >
           Delete
@@ -35,51 +35,51 @@ const Record = (props) => (
   </tr>
 );
 
-export default function RecordList() {
-  const [records, setRecords] = useState([]);
+export default function ProfileList() {
+  const [profiles, setProfiles] = useState([]);
 
-  // This method fetches the records from the database.
+  // This method fetches the profiles from the database.
   useEffect(() => {
-    async function getRecords() {
-      const response = await fetch(`http://localhost:5050/record/`);
+    async function getProfiles() {
+      const response = await fetch(`http://localhost:5050/profile/`);
       if (!response.ok) {
         const message = `An error occurred: ${response.statusText}`;
         console.error(message);
         return;
       }
-      const records = await response.json();
-      setRecords(records);
+      const profiles = await response.json();
+      setProfiles(profiles);
     }
-    getRecords();
+    getProfiles();
     return;
-  }, [records.length]);
+  }, [profiles.length]);
 
-  // This method will delete a record
-  async function deleteRecord(id) {
-    await fetch(`http://localhost:5050/record/${id}`, {
+  // This method will delete a profile
+  async function deleteProfile(id) {
+    await fetch(`http://localhost:5050/profile/${id}`, {
       method: "DELETE",
     });
-    const newRecords = records.filter((el) => el._id !== id);
-    setRecords(newRecords);
+    const newProfiles = profiles.filter((el) => el._id !== id);
+    setProfiles(newProfiles);
   }
 
-  // This method will map out the records on the table
-  function recordList() {
-    return records.map((record) => {
+  // This method will map out the profiles on the table
+  function profileList() {
+    return profiles.map((profile) => {
       return (
-        <Record
-          record={record}
-          deleteRecord={() => deleteRecord(record._id)}
-          key={record._id}
+        <Profile
+          profile={profile}
+          deleteProfile={() => deleteProfile(profile._id)}
+          key={profile._id}
         />
       );
     });
   }
 
-  // This following section will display the table with the records of individuals.
+  // This following section will display the table with the profiles of individuals.
   return (
     <>
-      <h3 className="text-lg font-semibold p-4">Employee Records</h3>
+      <h3 className="text-lg font-semibold p-4">Employee profiles</h3>
       <div className="border rounded-lg overflow-hidden">
         <div className="relative w-full overflow-auto">
           <table className="w-full caption-bottom text-sm">
@@ -100,7 +100,7 @@ export default function RecordList() {
               </tr>
             </thead>
             <tbody className="[&_tr:last-child]:border-0">
-              {recordList()}
+              {profileList()}
             </tbody>
           </table>
         </div>
@@ -108,4 +108,3 @@ export default function RecordList() {
     </>
   );
 }
-// save
